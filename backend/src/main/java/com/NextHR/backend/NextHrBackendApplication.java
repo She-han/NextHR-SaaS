2130db292430f1406fad22df8440b3f99fa421eb
Shehan Garusingarachchige <shehangarusinghe@gmail.com>
2025-10-28 22:55:56 +0530

    frontend and backend initialized succssfully

---
+++ backend/src/main/java/com/NextHR/backend/NextHrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.NextHR.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextHrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextHrBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/NextHR/backend/NextHrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NextHR.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextHrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
